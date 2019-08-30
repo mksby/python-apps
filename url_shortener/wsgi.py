18fca9219789a15c0e5983d8114327549a5f9b1c
MacPherson <maxim.troitsky@gmail.com>
2019-08-30 23:30:37 +0300

    update gitignore, wsgi

--- url_shortener/wsgi.py
+++ url_shortener/wsgi.py
@@ -12,7 +12,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "url_shortener.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
